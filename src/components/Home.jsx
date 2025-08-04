c57b08026bb7f34e1feaa260c658d388f5cb5073
ashishpaswnn <ashish969696100@gmail.com>
2025-08-04 15:45:25 +0530

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to BlueBazaar</h1>
+      <p>This is the homepage of your store.</p>
+    </div>
+  );
+};
+
+export default Home;
